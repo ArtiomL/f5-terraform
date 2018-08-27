dcf0e14e180713a5f2f154d6e13849e05fa0c257
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-08-27 23:28:23 +0300

    Update main.tf

--- cfg/lab/main.tf
+++ cfg/lab/main.tf
@@ -3,9 +3,11 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-	cidr_block = "10.0.0.0/16"
-	enable_dns_hostnames = true
 	tags {
 		Name = "test-vpc"
 	}
+	cidr_block = "10.0.0.0/16"
+	assign_generated_ipv6_cidr_block = true
+	enable_dns_support = true
+	enable_dns_hostnames = true
 }
