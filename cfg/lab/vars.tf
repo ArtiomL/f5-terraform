5c5b7ad598c21e8fb4f56ec80483e4ac31788a4e
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-08-27 23:50:17 +0300

    Input Variables

--- cfg/lab/vars.tf
+++ cfg/lab/vars.tf
@@ -1,3 +1,16 @@
+# Input Variables
+
 variable "aws_region" {
+	description = "AWS region"
 	default = "eu-central-1"
+}
+
+variable "tag_name" {
+	description = "Name tag"
+	default = "vpcF5Labs"
+}
+
+variable "cidr_block" {
+	description = "IPv4 CIDR block"
+	default = "10.200.0.0/16"
 }
