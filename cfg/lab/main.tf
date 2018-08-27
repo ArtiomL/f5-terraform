c7e9fb06ad56bb6fe9f57df8be8c4cf421f0f7e6
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-08-28 01:38:29 +0300

    Infrastructure

--- cfg/lab/main.tf
+++ cfg/lab/main.tf
@@ -49,3 +49,27 @@ resource "aws_internet_gateway" "gw" {
 		Name = "igw${var.tag_name}"
 	}
 }
+
+# Route table
+resource "aws_route_table" "public" {
+	vpc_id = "${aws_vpc.main.id}"
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = "${aws_internet_gateway.gw.id}"
+	}
+	tags {
+		Name = "rtPublic"
+  }
+}
+
+# Assign route table to management subnet
+resource "aws_route_table_association" "rta-mgmt" {
+	subnet_id = "${aws_subnet.mgmt.id}"
+	route_table_id = "${aws_route_table.public.id}"
+}
+
+# Assign route table to external subnet
+resource "aws_route_table_association" "rta-ext" {
+	subnet_id = "${aws_subnet.ext.id}"
+	route_table_id = "${aws_route_table.public.id}"
+}
