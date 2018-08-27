6c03583dccc3776962dec3bb750afbbbb7cda3ce
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-08-27 23:52:47 +0300

    Infrastructure

--- cfg/lab/main.tf
+++ cfg/lab/main.tf
@@ -1,12 +1,14 @@
+# Infrastructure
+
 provider "aws" {
 	region = "${var.aws_region}"
 }
 
 resource "aws_vpc" "main" {
 	tags {
-		Name = "test-vpc"
+		Name = "${var.tag_name}"
 	}
-	cidr_block = "10.0.0.0/16"
+	cidr_block = "${var.cidr_block}"
 	assign_generated_ipv6_cidr_block = true
 	enable_dns_support = true
 	enable_dns_hostnames = true
