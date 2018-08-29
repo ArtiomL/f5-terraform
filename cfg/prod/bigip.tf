54323d85beb07fabe5c28ad6e6901eec8b5dcc1d
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-08-30 02:02:05 +0300

    BIG-IP CloudFormation

--- cfg/prod/bigip.tf
+++ cfg/prod/bigip.tf
@@ -1,15 +1,19 @@
-# f5-existing-stack-byol-3nic-bigip
+# f5-existing-stack-across-az-cluster-byol-3nic-bigip
 resource "aws_cloudformation_stack" "bigip" {
 	name = "cf${var.tag_name}"
 	template_url = "${var.bigip_cft}"
 	parameters {
 		Vpc = "${aws_vpc.main.id}"
-		managementSubnetAz1 = "${aws_subnet.mgmt.id}"
-		subnet1Az1 = "${aws_subnet.ext.id}"
-		subnet2Az1 = "${aws_subnet.int.id}"
+		managementSubnetAz1 = "${aws_subnet.mgmt1.id}"
+		managementSubnetAz2 = "${aws_subnet.mgmt2.id}"
+		subnet1Az1 = "${aws_subnet.ext1.id}"
+		subnet1Az2 = "${aws_subnet.ext2.id}"
+		subnet2Az1 = "${aws_subnet.int1.id}"
+		subnet2Az2 = "${aws_subnet.int2.id}"
 		imageName = "AllTwoBootLocations"
 		instanceType = "m5.xlarge"
 		licenseKey1 = "${var.bigip_lic1}"
+		licenseKey2 = "${var.bigip_lic2}"
 		sshKey = "${aws_key_pair.main.id}"
 		restrictedSrcAddress = "${var.mgmt_asrc[0]}"
 		restrictedSrcAddressApp = "0.0.0.0/0"
