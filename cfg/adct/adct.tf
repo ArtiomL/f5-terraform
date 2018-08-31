3b5c5d52b5dec0fa47fd901b5818c9a5bebad90c
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-08-31 22:08:25 +0300

    Create adct.tf

---
+++ cfg/adct/adct.tf
@@ -0,0 +1,27 @@
+data "aws_ami" "ubuntu" {
+	most_recent = true
+	filter {
+		name   = "name"
+		values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+	}
+	filter {
+		name   = "virtualization-type"
+		values = ["hvm"]
+	}
+	owners = ["099720109477"] #Canonical
+}
+
+resource "aws_instance" "adct" {
+	ami = "${data.aws_ami.ubuntu.id}"
+	instance_type = "t2.micro"
+	key_name = "${aws_key_pair.main.id}"
+	subnet_id = "${aws_subnet.int.id}"
+	vpc_security_group_ids = ["${aws_security_group.mgmt.id}"]
+	associate_public_ip_address = true
+	source_dest_check = false
+	user_data = "${file("install.sh")}"
+	tags {
+		Name = "inADCT${count.index}"
+	}
+	count = 4
+}
