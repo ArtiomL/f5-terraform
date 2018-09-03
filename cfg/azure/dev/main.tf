de531bb2c0e23d6fffb14712ba085164cff4dd16
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-03 21:40:35 +0300

    Infrastructure

--- cfg/azure/dev/main.tf
+++ cfg/azure/dev/main.tf
@@ -5,7 +5,15 @@ provider "azurerm" {
 }
 
 # Resource group
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "main" {
 	name = "rg${var.tag_name}"
 	location = "${var.azure_region}"
+}
+
+# VNet
+resource "azurerm_virtual_network" "main" {
+	name = "vnet${var.tag_name}"
+	address_space = "${var.vnet_cidr}"
+	location = "${azurerm_resource_group.main.location}"
+	resource_group_name = "${azurerm_resource_group.main.name}"
 }
