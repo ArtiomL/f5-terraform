8682014a0c16126fcc0cc60838213b9615552d04
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-03 21:06:00 +0300

    Docker Wrapper Script

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 # f5-terraform - Docker Wrapper Script
 # https://github.com/ArtiomL/f5-terraform
 # Artiom Lichtenstein
-# v1.0.0, 27/08/2018
+# v1.0.1, 03/09/2018
 
 if [[ ! -z "$REPO" ]]; then
 	git clone "https://github.com/$REPO.git"
