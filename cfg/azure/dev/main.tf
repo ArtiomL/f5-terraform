94727b48ef0317315e0c944814b2c6b409effdb7
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-03 22:40:47 +0300

    Infrastructure

--- cfg/azure/dev/main.tf
+++ cfg/azure/dev/main.tf
@@ -16,4 +16,16 @@ resource "azurerm_virtual_network" "main" {
 	address_space = "${var.vnet_cidr}"
 	location = "${azurerm_resource_group.main.location}"
 	resource_group_name = "${azurerm_resource_group.main.name}"
+	subnet {
+		name = "snetMgmt"
+		address_prefix = "${var.mgmt_cidr}"
+	}
+	subnet {
+		name = "snetExternal"
+		address_prefix = "${var.ext_cidr}"
+	}
+	subnet {
+		name = "snetInternal"
+		address_prefix = "${var.int_cidr}"
+	}
 }
