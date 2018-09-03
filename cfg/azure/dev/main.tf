a2d292372817868c55c5f841222c4b0db27c0cd8
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-03 23:25:35 +0300

    Infrastructure

--- cfg/azure/dev/main.tf
+++ cfg/azure/dev/main.tf
@@ -16,16 +16,28 @@ resource "azurerm_virtual_network" "main" {
 	address_space = "${var.vnet_cidr}"
 	location = "${azurerm_resource_group.main.location}"
 	resource_group_name = "${azurerm_resource_group.main.name}"
-	subnet {
-		name = "snetMgmt"
-		address_prefix = "${var.mgmt_cidr}"
-	}
-	subnet {
-		name = "snetExternal"
-		address_prefix = "${var.ext_cidr}"
-	}
-	subnet {
-		name = "snetInternal"
-		address_prefix = "${var.int_cidr}"
-	}
+}
+
+# Management subnet
+resource "azurerm_subnet" "mgmt" {
+	name = "snetMgmt"
+	resource_group_name = "${azurerm_resource_group.main.name}"
+	virtual_network_name = "${azurerm_virtual_network.main.name}"
+	address_prefix = "${var.mgmt_cidr}"
+}
+
+# External subnet
+resource "azurerm_subnet" "ext" {
+	name = "snetExternal"
+	resource_group_name = "${azurerm_resource_group.main.name}"
+	virtual_network_name = "${azurerm_virtual_network.main.name}"
+	address_prefix = "${var.ext_cidr}"
+}
+
+# Internal subnet
+resource "azurerm_subnet" "int" {
+	name = "snetInternal"
+	resource_group_name = "${azurerm_resource_group.main.name}"
+	virtual_network_name = "${azurerm_virtual_network.main.name}"
+	address_prefix = "${var.int_cidr}"
 }
