6e2f90d722b6c05d726a09e2e0f74c2e47210737
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-05 22:28:02 +0300

    Docker Wrapper Script

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,13 +2,18 @@
 # f5-terraform - Docker Wrapper Script
 # https://github.com/ArtiomL/f5-terraform
 # Artiom Lichtenstein
-# v1.0.1, 03/09/2018
+# v1.0.2, 05/09/2018
 
+# Extensibility
 if [[ ! -z "$REPO" ]]; then
 	git clone "https://github.com/$REPO.git"
 	cd $(echo "$REPO" | cut -d"/" -f2)
 fi
 
+# Azure RM environment variables
 export $(cat /home/user/.azure/credentials | tr '\n' ' ')
+
+# GCP environment variables
+export GOOGLE_APPLICATION_CREDENTIALS=/home/user/.gcp/credentials
 
 exec /bin/sh
