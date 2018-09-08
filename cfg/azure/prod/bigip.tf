d0e85fb702a8f122af7ff15022a26ce1e4fabc72
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-08 21:13:51 +0300

    Create bigip.tf

---
+++ cfg/azure/prod/bigip.tf
@@ -0,0 +1,37 @@
+# https://github.com/F5Networks/f5-azure-arm-templates/tree/master/supported/standalone/3nic/existing-stack/byol/
+resource "azurerm_template_deployment" "bigip" {
+	name = "atd${var.tag_name}"
+	resource_group_name = "${azurerm_resource_group.main.name}"
+	template_body = "${replace(data.http.armt.body, "### START (INPUT) CUSTOM CONFIGURATION HERE\\n", "curl -sL https://f5labs.one/as3 | bash; tmsh modify /auth user ${var.bigip_user} password ${var.bigip_pass}")}"
+	parameters {
+		adminUsername = "${var.bigip_user}"
+		authenticationType = "sshPublicKey"
+		adminPasswordOrKey = "${file(var.key_path)}"
+		dnsLabel = "big-ip-a"
+		instanceName = "vmBIP-A"
+		instanceType = "Standard_DS3_v2"
+		imageName = "AllTwoBootLocations"
+		bigIpVersion = "latest"
+		licenseKey1 = "${var.bigip_lic1}"
+		#numberOfExternalIps = 1
+		vnetName = "${azurerm_virtual_network.main.name}"
+		vnetResourceGroupName = "${azurerm_resource_group.main.name}"
+		mgmtSubnetName = "${azurerm_subnet.mgmt.name}"
+		mgmtIpAddress = "${replace(var.mgmt_cidr, "0/24", "245")}"
+		externalSubnetName = "${azurerm_subnet.ext.name}"
+		externalIpAddressRangeStart = "${replace(var.ext_cidr, "0/24", "10")}"
+		internalSubnetName = "${azurerm_subnet.int.name}"
+		internalIpAddress = "${replace(var.int_cidr, "0/24", "245")}"
+		timeZone = "Asia/Jerusalem"
+		restrictedSrcAddress = "${var.mgmt_asrc[0]}"
+		allowUsageAnalytics = "No"
+	}
+	deployment_mode = "Incremental"
+}
+
+data "http" "armt" {
+	url = "${var.bigip_armt}"
+	request_headers {
+		"Accept" = "application/json"
+	}
+}
