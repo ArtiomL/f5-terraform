892d9a0eea952878ce4bd7233be31c28ca114d53
user <3167332+ArtiomL@users.noreply.github.com>
2018-09-13 21:14:16 -0400

    Remove duplicate directory

--- modules/aws/vpc/subnet/outputs.tf
+++
@@ -1,9 +0,0 @@
-# Subnet Output Variables
-
-output "ids" {
-  value = ["${aws_subnet.main.*.id}"]
-}
-
-output "sg_id" {
-  value = "${aws_security_group.main.id}"
-}
--- modules/aws/vpc/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-# VPC Output Variables
-
-output "id" {
-  value = "${aws_vpc.main.id}"
-}
-
-output "igw_id" {
-  value = "${element(concat(aws_internet_gateway.main.*.id, list("")), 0)}"
-}
