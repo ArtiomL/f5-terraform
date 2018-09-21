78b072dd895bcb1f7cf1d85be375398b2cb843b9
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-22 02:29:57 +0300

    Network Input Variables

--- modules/aws/network/variables.tf
+++ modules/aws/network/variables.tf
@@ -1,1 +1,41 @@
 # Network Input Variables
+
+variable "aws_region" {
+	description = "AWS region"
+	default = "eu-central-1"
+}
+
+
+# --- Tags --- #
+
+variable "tag_name" {
+	description = "Name tag"
+	default = "F5Labs"
+}
+
+variable "tag_environment" {
+	description = "Environment tag"
+	default = "DEV"
+}
+
+variable "tags_shared" {
+	description = "Other tags assigned to all resources"
+	type = "map"
+	default = {
+		Owner = "T.Durden"
+		BusinessUnit = "R&D"
+		CostCenter = "7741"
+		Project = "Mayhem"
+	}
+}
+
+
+# --- VPC Module --- #
+
+variable "vpc_cidr" {
+	description = "VPC IPv4 CIDR block"
+	default = "10.200.0.0/16"
+}
+
+
+# --- Subnet Module --- #
