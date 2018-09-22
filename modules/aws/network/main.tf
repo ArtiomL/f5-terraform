d773c42f5adaceb5147cee2127f9c824bc6c92fd
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-23 00:47:35 +0300

    Infrastructure

--- modules/aws/network/main.tf
+++ modules/aws/network/main.tf
@@ -12,3 +12,49 @@ module "vpc" {
 	tag_environment = "${var.tag_environment}"
 	tags_shared = "${var.tags_shared}"
 }
+
+# Public route table
+module "route" {
+	source = "./route"
+	vpc_id = "${module.vpc.id}"
+	gateway_id = "${module.vpc.igw_id}"
+	tag_name = "Public"
+	tag_environment = "${var.tag_environment}"
+	tags_shared = "${var.tags_shared}"
+}
+
+# Management subnets
+module "subnet_mgmt" {
+	source = "./subnet"
+	vpc_id = "${module.vpc.id}"
+	az_names = "${var.az_names}"
+	cidr_blocks = "${var.mgmt_cidrs}"
+	route_table_id = "${module.route.id}"
+	tag_name = "Mgmt"
+	tag_environment = "${var.tag_environment}"
+	tags_shared = "${var.tags_shared}"
+}
+
+# External subnets
+module "subnet_ext" {
+	source = "./subnet"
+	vpc_id = "${module.vpc.id}"
+	az_names = "${var.az_names}"
+	cidr_blocks = "${var.ext_cidrs}"
+	route_table_id = "${module.route.id}"
+	tag_name = "External"
+	tag_environment = "${var.tag_environment}"
+	tags_shared = "${var.tags_shared}"
+}
+
+# Internal subnets
+module "subnet_int" {
+	source = "./subnet"
+	vpc_id = "${module.vpc.id}"
+	az_names = "${var.az_names}"
+	cidr_blocks = "${var.int_cidrs}"
+	rt_associate = "false"
+	tag_name = "Internal"
+	tag_environment = "${var.tag_environment}"
+	tags_shared = "${var.tags_shared}"
+}
