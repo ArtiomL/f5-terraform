033ad0e750edc2db713c46d9b293d73f3b77699f
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-22 20:57:33 +0300

    Route Table Input Variables

--- modules/aws/network/route/variables.tf
+++ modules/aws/network/route/variables.tf
@@ -1,1 +1,30 @@
+# Route Table Input Variables
 
+variable "vpc_id" {
+	description = "VPC ID"
+}
+
+variable "gateway_id" {
+	description = "Internet Gateway ID"
+}
+
+variable "tag_name" {
+	description = "Route Table Name tag"
+	default = "F5Labs"
+}
+
+variable "tag_environment" {
+	description = "Route Table Environment tag"
+	default = "DEV"
+}
+
+variable "tags_shared" {
+	description = "Other tags assigned to all resources"
+	type = "map"
+	default = {
+		Owner = "T.Durden"
+		BusinessUnit = "R&D"
+		CostCenter = "7741"
+		Project = "Mayhem"
+	}
+}
