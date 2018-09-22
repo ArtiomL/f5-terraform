0ce79b41e81b97684db9719e010b9859f2c0b745
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-22 21:38:03 +0300

    Route Table Resources

---
+++ modules/aws/network/route/main.tf
@@ -0,0 +1,21 @@
+# Route Table Resources
+
+locals {
+	tags = {
+		CreatedBy = "Terraform"
+		Environment = "${var.tag_environment}"
+	}
+}
+
+resource "aws_route_table" "main" {
+	vpc_id = "${var.vpc_id}"
+	tags = "${merge(local.tags, var.tags_shared, map(
+		"Name", "rt${var.tag_name}${var.tag_environment}"
+	))}"
+}
+
+resource "aws_route" "main" {
+	route_table_id = "${aws_route_table.main.id}"
+	destination_cidr_block = "0.0.0.0/0"
+	gateway_id = "${var.gateway_id}"
+}
