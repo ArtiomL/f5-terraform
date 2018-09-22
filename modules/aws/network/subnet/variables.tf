9bb6472863531378e0ce2f40da54a6f681ba250b
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-22 04:15:02 +0300

    Subnet Input Variables

---
+++ modules/aws/network/subnet/variables.tf
@@ -0,0 +1,38 @@
+# Subnet Input Variables
+
+variable "vpc_id" {
+	description = "VPC ID"
+}
+
+variable "az_names" {
+	description = "Subnet Availability Zone list"
+	type = "list"
+	default = ["eu-central-1a", "eu-central-1b"]
+}
+
+variable "cidr_blocks" {
+	description = "Subnet IPv4 CIDR block list"
+	type = "list"
+	default = ["10.200.113.0/24", "10.200.123.0/24"]
+}
+
+variable "tag_name" {
+	description = "Subnet Name tag"
+	default = "Mgmt"
+}
+
+variable "tag_environment" {
+	description = "Subnet Environment tag"
+	default = "DEV"
+}
+
+variable "tags_shared" {
+	description = "Other tags assigned to all resources"
+	type = "map"
+	default = {
+		Owner = "T.Durden"
+		BusinessUnit = "R&D"
+		CostCenter = "7741"
+		Project = "Mayhem"
+	}
+}
