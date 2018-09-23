b9deb8b0ecead4261d909b2df109a27bbb30db97
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-24 01:09:04 +0300

    VPC Input Variables

--- modules/aws/network/vpc/variables.tf
+++ modules/aws/network/vpc/variables.tf
@@ -1,47 +1,47 @@
 # VPC Input Variables
 
 variable "cidr_block" {
-	description = "VPC IPv4 CIDR block"
-	default = "10.200.0.0/16"
+  description = "VPC IPv4 CIDR block"
+  default     = "10.200.0.0/16"
 }
 
 variable "ipv6_cidr_block" {
-	description = "Associate an IPv6 CIDR block with the VPC"
-	default = true
+  description = "Associate an IPv6 CIDR block with the VPC"
+  default     = true
 }
 
 variable "enable_dns_support" {
-	description = "Enable DNS support in the VPC"
-	default = true
+  description = "Enable DNS support in the VPC"
+  default     = true
 }
 
 variable "enable_dns_hostnames" {
-	description = "Enable DNS hostnames in the VPC"
-	default = true
+  description = "Enable DNS hostnames in the VPC"
+  default     = true
 }
 
 variable "create_igw" {
-	description = "Attach an internet gateway to the VPC"
-	default = true
+  description = "Attach an internet gateway to the VPC"
+  default     = true
 }
 
 variable "tag_name" {
-	description = "VPC Name tag"
-	default = "F5Labs"
+  description = "VPC Name tag"
+  default     = "F5Labs"
 }
 
 variable "tag_environment" {
-	description = "VPC Environment tag"
-	default = "DEV"
+  description = "VPC Environment tag"
+  default     = "DEV"
 }
 
 variable "tags_shared" {
-	description = "Other tags assigned to all resources"
-	type = "map"
-	default = {
-		Owner = "T.Durden"
-		BusinessUnit = "R&D"
-		CostCenter = "7741"
-		Project = "Mayhem"
-	}
+  description = "Other tags assigned to all resources"
+  type        = "map"
+  default = {
+    Owner        = "T.Durden"
+    BusinessUnit = "R&D"
+    CostCenter   = "7741"
+    Project      = "Mayhem"
+  }
 }
