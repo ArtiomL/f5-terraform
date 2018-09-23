d62fa2b88ba1f716e3ad206d91273596527df0f1
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-24 02:27:06 +0300

    Infrastructure

--- env/aws/dev/main.tf
+++ env/aws/dev/main.tf
@@ -4,21 +4,16 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-# AWS Network
+# AWS network
 module "aws_network" {
   source          = "github.com/ArtiomL/f5-terraform/modules/aws/network"
   aws_region      = "${var.aws_region}"
   vpc_cidr        = "${var.vpc_cidr}"
-  mgmt1_cidr      = "${var.mgmt_cidr}"
-  ext1_cidr       = "${var.ext_cidr}"
-  int1_cidr       = "${var.int_cidr}"
-  mgmt2_cidr      = ""
-  ext2_cidr       = ""
-  int2_cidr       = ""
+  az_names        = "${var.az_names}"
+  mgmt_cidrs      = "${var.mgmt_cidrs}"
+  ext_cidrs       = "${var.ext_cidrs}"
+  int_cidrs       = "${var.int_cidrs}"
   tag_name        = "${var.tag_name}"
   tag_environment = "${var.tag_environment}"
-  tag_owner       = "${var.tag_owner}"
-  tag_bu          = "${var.tag_bu}"
-  tag_costcenter  = "${var.tag_costcenter}"
-  tag_project     = "${var.tag_project}"
+  tags_shared     = "${var.tags_shared}"
 }
