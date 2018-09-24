0341bdadb258260adc8a588d0545ff0fb4bb55b9
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-25 01:25:55 +0300

    Public IP Output Variables

--- modules/common/ifconfig/outputs.tf
+++ modules/common/ifconfig/outputs.tf
@@ -4,6 +4,10 @@ output "ip" {
   value = "${data.external.ifconfig.result.ip}"
 }
 
+output "ip_cidr" {
+  value = "${data.external.ifconfig.result.ip}/32"
+}
+
 output "map" {
   value = "${data.external.ifconfig.result}"
 }
