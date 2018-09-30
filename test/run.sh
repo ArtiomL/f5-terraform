1ea9b4c8c6f6db48d4e77c3b887f5fcf28834fe1
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-09-30 16:15:55 +0300

    terraform init

--- test/run.sh
+++ test/run.sh
@@ -2,16 +2,16 @@
 # f5-terraform - Run Tests
 # https://github.com/ArtiomL/f5-terraform
 # Artiom Lichtenstein
-# v1.0.4, 09/09/2018
+# v1.0.5, 30/09/2018
 
 set -xeo pipefail
 
 REPO="artioml/f5-terraform"
 
 # Terraform
-str_TEST="terraform --version;"
-str_TEST="$str_TEST cp -n cfg/aws/dev/* cfg/aws/adct/;"
-str_TEST="$str_TEST find cfg/*/* -type d -print0 | xargs -0 -n1 -t terraform init"
+str_TEST="terraform --version; \
+	cp -n cfg/aws/dev/* cfg/aws/adct/; \
+	find cfg/ env/ modules/ -type d -print0 | xargs -0 -n1 -t terraform init"
 
 if [ "$TRAVIS" == "true" ]; then
 	docker run $REPO /bin/sh -c "$str_TEST"
