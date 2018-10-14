824a9412b0080f315610932af23bf6e11138d5fb
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-10-14 23:20:55 +0300

    Public IP Data Source

--- modules/common/ifconfig/main.tf
+++ modules/common/ifconfig/main.tf
@@ -1,5 +1,5 @@
 # Public IP Data Source
 
 data "external" "ifconfig" {
-  program = ["sh", "-c", "wget -qO- ifconfig.co/json | jq '{ip, country, country_iso, city, hostname}'"]
+  program = ["sh", "-c", "curl -s https://ifconfig.co/json | jq '{ip, country, country_iso, city, hostname}'"]
 }
