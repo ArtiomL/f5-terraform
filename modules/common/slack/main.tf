e43d09835b8e20a3059b36a2f544ddc07173237b
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-10-14 15:17:36 +0300

    Slack Notifications

--- modules/common/slack/main.tf
+++ modules/common/slack/main.tf
@@ -2,6 +2,6 @@
 
 resource "null_resource" "main" {
   provisioner "local-exec" {
-    command = "curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"${var.message}\"}' https://hooks.slack.com/services/${var.webhook_url}"
+    command = "curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"${var.message_text}\"}' https://hooks.slack.com/services/${var.webhook_url}"
   }
 }
