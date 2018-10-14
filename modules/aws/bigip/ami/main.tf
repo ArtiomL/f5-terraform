23a1b869d57d61b2016d9513a2205800a223aac8
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-10-14 23:29:39 +0300

    AMI Data Source

--- modules/aws/bigip/ami/main.tf
+++ modules/aws/bigip/ami/main.tf
@@ -3,5 +3,5 @@
 data "aws_region" "this" {}
 
 data "external" "ami" {
-  program = ["sh", "-c", "wget -qO- ${var.ami_url} | jq 'with_entries(.value |= .${var.image_name})'"]
+  program = ["sh", "-c", "curl -sL ${var.ami_url} | jq 'with_entries(.value |= .${var.image_name})'"]
 }
