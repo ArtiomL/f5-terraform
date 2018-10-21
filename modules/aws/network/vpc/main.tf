468c6ed4b85591eb807a0d15f7a7c4c05939727f
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-10-21 11:01:54 +0300

    VPC Resources

--- modules/aws/network/vpc/main.tf
+++ modules/aws/network/vpc/main.tf
@@ -12,6 +12,7 @@ resource "aws_vpc" "main" {
   assign_generated_ipv6_cidr_block = "${var.ipv6_cidr_block}"
   enable_dns_support               = "${var.enable_dns_support}"
   enable_dns_hostnames             = "${var.enable_dns_hostnames}"
+
   tags = "${merge(local.tags, var.tags_shared, map(
     "Name", "vpc${var.tag_name}${var.tag_environment}"
   ))}"
@@ -20,6 +21,7 @@ resource "aws_vpc" "main" {
 resource "aws_internet_gateway" "main" {
   count  = "${var.create_igw ? 1 : 0}"
   vpc_id = "${aws_vpc.main.id}"
+
   tags = "${merge(local.tags, var.tags_shared, map(
     "Name", "igw${var.tag_name}${var.tag_environment}"
   ))}"
