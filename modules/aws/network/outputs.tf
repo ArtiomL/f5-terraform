986cef3d605dd86354f40802747247d33a8b7f1a
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-10-24 15:31:28 +0300

    Network Output Variables

--- modules/aws/network/outputs.tf
+++ modules/aws/network/outputs.tf
@@ -8,10 +8,22 @@ output "subnet_mgmt_ids" {
   value = "${module.subnet_mgmt.ids}"
 }
 
+output "subnet_mgmt_sg_id" {
+  value = "${module.subnet_mgmt.sg_id}"
+}
+
 output "subnet_ext_ids" {
   value = "${module.subnet_ext.ids}"
 }
 
+output "subnet_ext_sg_id" {
+  value = "${module.subnet_ext.sg_id}"
+}
+
 output "subnet_int_ids" {
   value = "${module.subnet_int.ids}"
+}
+
+output "subnet_int_sg_id" {
+  value = "${module.subnet_int.sg_id}"
 }
